4da514d39f716e80fa96612ea0914f15c91931a5
User <User@User-PC>
2022-10-19 16:29:42 +0300

    HW - 32

---
+++ src/telran/io/CopyFilesInputOutputStreams.java
@@ -0,0 +1,51 @@
+package telran.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Application for copying files based on FileInputStream and FileOutputStream
+ * files may be very big (several Gigs)
+ * args[0] - source file path
+ * args[1] - destination file path
+ * args[2] - if exists "overwritten" then destination may be overwritten otherwise may not be
+ * 
+ * Outout one of the following:
+ * Files have been copied (<amount of bytes>  <time of copieng>)
+ * Source file doesn't exist
+ * Destination can not be overwritten
+ *
+ */
+public class CopyFilesInputOutputStreams {
+
+
+	private static final int BUFFER_SIZE = 1024;
+
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("Please provide input and output files");
+			System.exit(0);
+		}
+
+		String inputFile = args[0];
+		String outputFile = args[1];
+
+		try (InputStream inputStream = new FileInputStream(inputFile);
+				OutputStream outputStream = new FileOutputStream(outputFile);) {
+
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int bytesRead = -1;
+
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
+
---
+++ src/telran/io/FileTests.java
@@ -0,0 +1,67 @@
+package telran.io;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.*;
+
+class FileTests {
+	File file;
+
+	@BeforeEach
+	void setUp() {
+		file = new File("dir1/dir2");
+		file.delete();
+	}
+
+	@Test
+	void newDirectoryTest() {
+		assertFalse(file.exists());
+		file.mkdirs();
+		assertTrue(file.exists());
+	}
+
+	@Test
+	void printDirectoryContent() throws Exception {
+		printDirectory("C:bin\\telran", 6);
+		printDirectory("..\\hello", 2);
+		printDirectory("..", 1);
+		assertThrows(IllegalArgumentException.class, () -> printDirectory("C:", -1));
+		assertThrows(IOException.class, () -> printDirectory("X:", 5));
+	}
+
+	/**
+	 * 
+	 * @param pathName - name of path to initial directory
+	 * @param level    - level of sub-directories printing example: 
+	 * level=1 printing only first level of the initial directory content      
+	 * level=2 content + sub-directories content ... 
+	 * level=-1 printing  ??????             
+	 *                    
+	 * @throws Exception
+	 * 
+	 */
+	private void printDirectory(String pathName, int level) throws Exception {
+		/*
+		 * <dir> type=dir 
+		 * 		<dir> type=dir <file> type=file 
+		 * 				<dir> type=file ...
+		 */
+
+		Path start = Paths.get(pathName);
+
+		try (Stream<Path> pathStream = Files.walk(start, level)) {
+			pathStream.forEach(System.out::println);
+
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(String.format("%d level does not exist\n", level));
+		} catch (IOException e) {
+			throw new IOException(String.format("%s directory does not exist\n", pathName));
+		}
+	}
+}
